08cf5bd415a2948a4dcbcef62b844ac470481b3d
gabrielbygas <gabrielbygas@gmail.com>
2026-01-10 21:48:43 +0400

    factory_and_seeders

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasUuids;
+    use HasFactory;
 
     protected $fillable = ['name', 'price', 'stock_quantity'];
     protected $casts = [
